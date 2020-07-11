35e81877657c7fc8a23e68d975b488dd857a1285
Pablo Dávila Herrero <Pablo-Davila@users.noreply.github.com>
2020-07-11 22:40:55 +0200

    Remove log writing

--- psychologist.py
+++ psychologist.py
@@ -4,29 +4,23 @@ import sys
 
 therapist = eliza.eliza()
 bot = telebot.TeleBot(sys.argv[1])
-
-from datetime import datetime
-
-now = datetime.now()
-current_time = now.strftime("%Y-%m-%d_%H-%M-%S")
-with open("log\log_"+current_time+".txt","w+") as f:
 	
-	@bot.message_handler(commands=['start', 'help'])
-	def send_welcome(message):
-		user = message.from_user
-		bot.reply_to(message, "Hello " + user.first_name + " " + user.last_name + ".\nHow are you feeling today? Please answer in English.")
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	'''Welcome message.'''
+	user = message.from_user
+	bot.reply_to(message, "Hello " + user.first_name + " " + user.last_name + ".\nHow are you feeling today? Please answer me in English.")
 
-	@bot.message_handler(func=lambda message: message.text!="/start")
-	def echo_all(message):
-		cid = message.chat.id
-		
-		reply = therapist.respond(message.text)
-		bot.send_message(cid, reply)
-		
-		user = message.from_user
-		name = str(user.first_name) + " " + str(user.last_name)
-		f.write(name + ": " + message.text + "\n")
-		f.write("TO " + name + " " + user.last_name + ": " + reply + "\n")
+@bot.message_handler(func=lambda message: message.text!="/start")
+def echo_all(message):
+	'''Answer for each new message.'''
+	cid = message.chat.id
+	
+	reply = therapist.respond(message.text)
+	bot.send_message(cid, reply)
+	
+	user = message.from_user
+	name = str(user.first_name) + " " + str(user.last_name)
 
-	print("Running MyElizaPsychologistBot.py")
-	bot.polling()
+print("Running MyElizaPsychologistBot.py")
+bot.polling()
